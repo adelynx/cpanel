a098c80fbf5e1ebd8067f490b8c9023bd0673694
Adel KEDJOUR <a.kedjour@corex.dz>
2013-06-10 05:32:14 +0100

    Create a facade
    Copy the file "Cpanel.php" into
    "you_project_dir\vendor\laravel\framework\src\Illuminate\Support\Facades".

---
+++ Facades/Cpanel.php
@@ -0,0 +1,12 @@
+<?php namespace Illuminate\Support\Facades;
+
+class Cpanel extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'cpanel'; }
+
+}
